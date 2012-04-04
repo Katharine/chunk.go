28e883b99ed76bc6648b6edbc03c16899f77d82b
Katharine Berry <katharine@kathar.in>
2012-04-04 00:50:18 -0400

    Initial commit. Imitates Python's chunk module.

---
+++ chunk.go
@@ -0,0 +1,94 @@
+// chunk provides an interface for reading IFF data files.
+// The API is based on the Python standard library's "chunk" module.
+package chunk
+
+import "io"
+import "errors"
+
+// A Chunk represents a single chunk in an IFF file.
+// Chunks implement the io.Reader and io.Seeker interfaces.
+type Chunk struct {
+	file   io.ReadSeeker
+	id     string
+	size   uint32
+	base   uint32
+	offset uint32
+}
+
+// Returns a new chunk. An instance of Chunk is specifically allowed as the
+// argument to Make. This is used to read chunks inside other chunks.
+func Make(f io.ReadSeeker) (*Chunk, error) {
+	id := make([]byte, 4)
+	if n, err := f.Read(id); n != 4 || err != nil {
+		return nil, err
+	}
+	sizeBits := make([]byte, 4)
+	if n, err := f.Read(sizeBits); n != 4 || err != nil {
+		return nil, err
+	}
+	size := (uint32(sizeBits[0]) << 24) | (uint32(sizeBits[1]) << 16) | (uint32(sizeBits[2]) << 8) | uint32(sizeBits[3])
+	base, _ := f.Seek(0, 1)
+	return &Chunk{f, string(id), size, uint32(base), 0}, nil
+}
+
+// Returns the name (ID) of the chunk. This is the first 4 bytes of the chunk.
+func (this *Chunk) Name() (id string) {
+	return this.id
+}
+
+// Returns the size of the chunk.
+func (this *Chunk) Size() (size uint32) {
+	return this.size
+}
+
+// Seek implements the io.Seeker interface.
+func (this *Chunk) Seek(offset int64, whence int) (ret int64, err error) {
+	switch whence {
+	case 1:
+		offset += int64(this.offset)
+	case 2:
+		offset += int64(this.size)
+	}
+	if offset < 0 || offset > int64(this.size) {
+		return int64(this.offset), errors.New("Invalid seek offset")
+	}
+	pos, err := this.file.Seek(int64(this.base)+offset, 0)
+	if err != nil {
+		return int64(pos) - int64(this.base), err
+	}
+	this.offset = uint32(offset)
+	return offset, nil
+}
+
+// Skips to the end of the chunk. All further reads will return io.EOF.
+// If you are not inrested in the contents of the chunk, this method should be called
+// so that the file points to the start of the next chunk.
+func (this *Chunk) Skip() {
+	pos := this.base + this.size
+	// We must always start on even boundaries
+	if pos&1 == 1 {
+		pos++
+	}
+	this.offset = this.size
+	this.file.Seek(int64(pos), 0)
+}
+
+// Read implements the io.Reader interface.
+func (this *Chunk) Read(buffer []byte) (n int, err error) {
+	if this.offset == this.size {
+		return 0, io.EOF
+	}
+	size := uint32(len(buffer))
+	if size > this.size-this.offset {
+		size = this.size - this.offset
+		buffer = buffer[:size]
+	}
+	n, err = this.file.Read(buffer)
+	this.offset += uint32(n)
+	return
+}
+
+// Tells you if the chunk is a TTY. Hint: it isn't.
+func (this *Chunk) IsTTY() bool {
+	return false
+}
---
+++ chunk_test.go
@@ -0,0 +1,186 @@
+package chunk
+
+import "testing"
+import "bytes"
+import "io"
+
+var testString = []byte{
+	'T', 'E', 'S', 'T', 0, 0, 0, 2, 42, 24,
+	'F', 'O', 'O', ' ', 0, 0, 0, 20,
+	'I', 'N', '1', ' ', 0, 0, 0, 1, 255, 0,
+	'I', 'N', '2', ' ', 0, 0, 0, 2, 127, 129,
+}
+
+func TestHeader(t *testing.T) {
+	f := bytes.NewReader(testString)
+
+	chunk, err := Make(f)
+	if err != nil {
+		t.Errorf("Failed to create Chunk: '%s'", err)
+	}
+
+	if name := chunk.Name(); name != "TEST" {
+		t.Errorf("Chunk name '%s' != 'TEST'", name)
+	}
+
+	if size := chunk.Size(); size != 2 {
+		t.Errorf("Chunk size %d != 2.", size)
+	}
+
+	// Try a multi-byte size.
+	// NOTE: Invalid file.
+	f = bytes.NewReader([]byte{'T', 'E', 'S', 'T', 0x12, 0x34, 0x56, 0x78})
+	chunk, _ = Make(f)
+	if size := chunk.Size(); size != 305419896 {
+		t.Errorf("Multi-byte size %d != 305419896", size)
+	}
+}
+
+func TestBasicRead(t *testing.T) {
+	f := bytes.NewReader(testString)
+
+	chunk, _ := Make(f)
+
+	buffer := make([]byte, 10)
+	if n, err := chunk.Read(buffer); n != 2 || err != nil {
+		t.Errorf("Failed reading 2 bytes; got %d (%s)", n, err)
+	}
+
+	if buffer[0] != 42 || buffer[1] != 24 {
+		t.Error("Invalid data in output.")
+	}
+
+	// Check that we can't read any more.
+	if n, err := chunk.Read(buffer); n > 0 || err != io.EOF {
+		t.Errorf("Didn't get EOF when reading past end of buffer (read %d bytes: %s).", n, buffer)
+	}
+}
+
+func TestSkip(t *testing.T) {
+	f := bytes.NewReader(testString)
+
+	chunk, _ := Make(f)
+	chunk.Skip()
+
+	// Check that we can't read it.
+	buffer := make([]byte, 10)
+	if n, err := chunk.Read(buffer); n > 0 || err != io.EOF {
+		t.Errorf("Didn't get EOF when reading skipped chunk (read %d bytes: %s).", n, buffer)
+	}
+
+	// Check that we've advanced to the *next* chunk.
+	if chunkFoo, err := Make(f); err == nil {
+		if name := chunkFoo.Name(); name != "FOO " {
+			t.Errorf("Next chunk not called \"FOO \" (got \"%s\")", name)
+		}
+	} else {
+		t.Errorf("Error when creating chunk after skipping previous chunk: %s", err)
+	}
+}
+
+func checkByte(chunk *Chunk, expected byte, t *testing.T) {
+	buffer := make([]byte, 1)
+	if n, err := chunk.Read(buffer); n != 1 || err != nil {
+		t.Errorf("Failed to read one byte (got %d): %s", n, err)
+	} else {
+		if buffer[0] != expected {
+			t.Errorf("Didn't read expected byte (expecting %d, got %d)", expected, buffer[0])
+		}
+	}
+}
+
+func TestSeek(t *testing.T) {
+	f := bytes.NewReader(testString)
+	chunkTest, _ := Make(f)
+	chunkTest.Skip()
+	chunkFoo, _ := Make(f)
+
+	// This test will ignore the internal structure of FOO  and simply check that we can seek through it.
+
+	// Relative seek:
+	if pos, err := chunkFoo.Seek(2, 1); pos != 2 || err != nil {
+		t.Errorf("Failed to seek to position 2 (at %d): %s", pos, err)
+	}
+
+	// Read a byte; check it.
+	checkByte(chunkFoo, '1', t)
+
+	// Skip relative again.
+	if pos, err := chunkFoo.Seek(9, 1); pos != 12 || err != nil {
+		t.Errorf("Failed relative seek; at %d; expecting 12 (%s)", pos, err)
+	}
+
+	// Check the byte again.
+	checkByte(chunkFoo, '2', t)
+
+	// Try relative skipping past the end.
+	if _, err := chunkFoo.Seek(20, 1); err == nil {
+		t.Error("Seeked past the end; expected EOF")
+	}
+
+	// Try an absolute seek
+	if pos, err := chunkFoo.Seek(8, 0); pos != 8 || err != nil {
+		t.Errorf("Failed to seek to position 8 (at %d): %s", pos, err)
+	}
+
+	// Check the byte
+	checkByte(chunkFoo, 255, t)
+
+	// Try an absolute seek from the end
+	if pos, err := chunkFoo.Seek(-3, 2); pos != 17 || err != nil {
+		t.Errorf("Failed to seek to position end-3 (at %d): %s", pos, err)
+	}
+
+	// Check a byte
+	checkByte(chunkFoo, 2, t)
+
+	// Try a relative seek to exactly the end.
+	if pos, err := chunkFoo.Seek(1, 1); pos != 19 || err != nil {
+		t.Errorf("Failed relative seek; at %d; expecting 19 (%s)", pos, err)
+	}
+
+	// Read the last byte.
+	checkByte(chunkFoo, 129, t)
+}
+
+// Test if we can read chunks inside chunks
+func TestSubChunks(t *testing.T) {
+	f := bytes.NewReader(testString)
+	chunkTest, _ := Make(f)
+	chunkTest.Skip()
+	chunkFoo, _ := Make(f)
+
+	chunkIN1, err := Make(chunkFoo)
+	if err != nil {
+		t.Errorf("Failed to create chunkIN1 from chunkFoo: %s", err)
+	}
+
+	if name := chunkIN1.Name(); name != "IN1 " {
+		t.Errorf("chunkIN1 name is '%s'; expected 'IN1 '", name)
+	}
+
+	chunkIN1.Skip()
+	chunkIN2, err := Make(chunkFoo)
+
+	if err != nil {
+		t.Errorf("Failed to create chunkIN2 from chunkFoo: %s", err)
+	}
+
+	if name := chunkIN2.Name(); name != "IN2 " {
+		t.Errorf("chunkIN2 name is '%s'; expected 'IN2 '", name)
+	}
+
+	if pos, err := chunkIN2.Seek(1, 1); pos != 1 || err != nil {
+		t.Errorf("Failed to seek to position 1 in chunkIN2; at %d: %s", pos, err)
+	}
+
+	checkByte(chunkIN2, 129, t)
+}
+
+func TestTTY(t *testing.T) {
+	f := bytes.NewReader(testString)
+	chunk, _ := Make(f)
+	if chunk.IsTTY() {
+		t.Error("Chunk is apparently a TTY!?")
+	}
+}
